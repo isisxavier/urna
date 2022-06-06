62f8ce135c8809cd8e71203e220e48de398eeed6
Isis <isisxavierti@gmail.com>
2022-06-06 03:12:57 -0300

    tentando add js

---
+++ js/armazenar.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form')
+const voto = document.getElementById('numero')
+
+function votar(){
+
+    if(numero.value == 13){
+        alert('isis')
+    } else if(numero.value == 12){
+        alert('brenda')
+    }
+}
