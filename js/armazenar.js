c46e635c58b0c96765a11c0bdb8b55d955ddedbb
Isis <isisxavierti@gmail.com>
2022-06-06 04:06:24 -0300

    sei la

--- js/armazenar.js
+++ js/armazenar.js
@@ -2,10 +2,17 @@ const form = document.getElementById('form')
 const voto = document.getElementById('numero')
 
 function votar(){
-
-    if(numero.value == 13){
+    
+    if(numero.value == 10){
         alert('isis')
-    } else if(numero.value == 12){
+    } else if(numero.value == 11){
         alert('brenda')
     }
+    else if(numero.value == 12){
+        alert('caio')
+    }
+    else{
+        alert('voto nulo')
+    }
+ 
 }
